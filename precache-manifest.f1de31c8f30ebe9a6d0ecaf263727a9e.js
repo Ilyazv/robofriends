cb1611da1a9ede0c614f249a5eb3505479f6dcf7
ilyaz <zvirenko@gmail.com>
2019-05-09 16:19:53 +0300

    Updates

---
+++ precache-manifest.f1de31c8f30ebe9a6d0ecaf263727a9e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "37b746006cb2a6bb6fe2cb2c60936cb4",
+    "url": "/robofriends/index.html"
+  },
+  {
+    "revision": "8d8ad546c8bba8b27b11",
+    "url": "/robofriends/static/css/2.31dafeb2.chunk.css"
+  },
+  {
+    "revision": "2438c823a84e75d82397",
+    "url": "/robofriends/static/css/main.a5c98c3c.chunk.css"
+  },
+  {
+    "revision": "8d8ad546c8bba8b27b11",
+    "url": "/robofriends/static/js/2.c05ccf0c.chunk.js"
+  },
+  {
+    "revision": "2438c823a84e75d82397",
+    "url": "/robofriends/static/js/main.6e2191a5.chunk.js"
+  },
+  {
+    "revision": "143eb19aa518302daf32",
+    "url": "/robofriends/static/js/runtime~main.fb41d28d.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robofriends/static/media/SEGA.12a3db8b.woff"
+  }
+]);
